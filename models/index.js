0a6f1f064caced75cc57a439dc74c18f577f33f3
Mariat28 <mariatndagire28@gmail.com>
2024-10-04 12:40:21 +0300

    send OTP via gmail

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
-// models/index.js
 const User = require('./user');
 const Role = require('./role');
 
-// Define associations
 User.belongsTo(Role, { foreignKey: 'role_id' });
 Role.hasMany(User, { foreignKey: 'role_id' });
 
